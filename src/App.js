09209cc359a157eedaec8392a87da818998187ec
parmarraj999 <rajraja5122@gmail.com>
2025-03-17 17:34:18 +0530

    theme toggle function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import { BrowserRouter, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './client-side/pages/home/home';
+import { ThemeProvider } from './client-side/context/ThemeContext';
+import './client-side/styles/theme/theme.css'
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Router>
-          <Routes/>
-        </Router>
-      </BrowserRouter>
-    </div>
+    <ThemeProvider>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/client-side/pages/home/home.js
+++ src/client-side/pages/home/home.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../../context/ThemeContext'
+// import "../../styles/theme/theme.css";
+import './home.css'
 
 function Home() {
-  return (
-    <div  >
 
+  const { theme , toggleTheme } = useContext(ThemeContext)
+
+  return (
+    <div className='home-container' >
+      <h1>Home {theme}</h1>
+      <button onClick={toggleTheme}>Toggle Theme</button>
     </div>
   )
 }
